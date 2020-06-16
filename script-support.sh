a41b35613a32e1f0e72c3f9ef96ce2c20e655967
Paul Prestopnik <prestop@ucar.edu>
2020-06-16 23:19:30 +0000

    initial commit of some useful stuff

---
+++ script-support.sh
@@ -0,0 +1,24 @@
+
+
+# Usage:  call this after a shell command if you want to make sure it succeeded, and exit with a message if it failed
+
+# grep $user2install /etc/passwd >/dev/null
+# check_return_code "Missing user: $user2install"
+check_return_code() {
+    if [ $? -ne 0 ]; then
+	exit_with_message "$1"
+    fi
+}
+
+# Usage: Call this when you want to print a message and exit:
+
+#if [ ! -d $installDir ]; then
+#    exit_with_message "$installDir does not appear to be mounted on this system"
+#fi
+
+exit_with_message() {
+    printf "EXITING  Error message is:   %s" $1
+    exit 1
+}
+
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+printf "Setting up ~prestop environment\n"
+
+# find location of this running script
+SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))  # https://stackoverflow.com/a/34208365/
+
+# include utility functions
+. $SCRIPT_DIR/script-support.sh
+
+
+[ ! -f ~/.bashrc ] || mv ~/.bashrc ~/.bashrc-original
+check_return_code "Error install .bashrc"
