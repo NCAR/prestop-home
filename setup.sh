908c6e1314fc5fc369b34fdc7b5bc4526e772402
Paul Prestopnik <prestop@ucar.edu>
2020-06-16 23:22:58 +0000

    fixed shebang, and added successful completion message

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 printf "Setting up ~prestop environment\n"
 
@@ -11,3 +11,6 @@ SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))  #
 
 [ ! -f ~/.bashrc ] || mv ~/.bashrc ~/.bashrc-original
 check_return_code "Error install .bashrc"
+
+
+print "Setup of ~prestop completed succesfully\n"
