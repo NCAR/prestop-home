52f90f931094154ee3e20bf400860b2fb5e97b48
Paul Prestopnik <prestop@ucar.ucar.edu>
2020-06-17 11:15:38 -0600

    lots more machines added from asimov bashrc

--- ssh-help.sh
+++ ssh-help.sh
@@ -31,7 +31,6 @@ function centennial() {
   ssh atec4dwx@centennial11.arl.hpc.mil
 }
 
-
 function atec-exposed() {
   ssh atec-exposed.rap.ucar.edu
 }
@@ -55,3 +54,145 @@ function data-access() {
 function atec-dataops4() {
   ssh atec-dataops4.rap.ucar.edu
 }
+
+function deere {
+    ssh  deere.rap.ucar.edu
+}
+
+function thyme {
+    ssh  thyme.rap.ucar.edu
+}
+
+function fip-faa {
+    ssh  fip-faa@sonbef.rap.ucar.edu
+}
+
+function cip-faa {
+    ssh  cip-faa@sonbef.rap.ucar.edu
+}
+
+function fip-dev {
+    ssh  fip-dev@sonbef.rap.ucar.edu
+}
+
+function cip-dev {
+    ssh  cip-dev@sonbef.rap.ucar.edu
+}
+
+function curry {
+    ssh  curry.rap.ucar.edu
+}
+
+function oregano {
+    ssh  oregano.rap.ucar.edu
+}
+
+function parsley {
+    ssh  parsley.rap.ucar.edu
+}
+
+function sonbef {
+    ssh  sonbef.rap.ucar.edu
+}
+
+function khaba {
+    ssh  khaba.rap.ucar.edu
+}
+
+function raneb {
+    ssh  raneb.rap.ucar.edu
+}
+
+function huni {
+    ssh  huni.rap.ucar.edu
+}
+
+function floyd {
+    ssh  floyd.rap.ucar.edu
+}
+
+function ahmose {
+    ssh  ahmose.rap.ucar.edu
+}
+
+function ncardas1 {
+    ssh  ncardas1.rap.ucar.edu
+}
+
+function ncardas2 {
+    ssh  ncardas2.rap.ucar.edu
+}
+
+function deere-y {
+    ssh -Y deere.rap.ucar.edu
+}
+
+function thyme-y {
+    ssh -Y thyme.rap.ucar.edu
+}
+
+function fip-faa-y {
+    ssh -Y fip-faa@sonbef.rap.ucar.edu
+}
+
+function cip-faa-y {
+    ssh -Y cip-faa@sonbef.rap.ucar.edu
+}
+
+function fip-dev-y {
+    ssh -Y fip-dev@sonbef.rap.ucar.edu
+}
+
+function cip-dev-y {
+    ssh -Y cip-dev@sonbef.rap.ucar.edu
+}
+
+function curry-y {
+    ssh -Y curry.rap.ucar.edu
+}
+
+function oregano-y {
+    ssh -Y oregano.rap.ucar.edu
+}
+
+function parsley-y {
+    ssh -Y parsley.rap.ucar.edu
+}
+
+function sonbef-y {
+    ssh -Y sonbef.rap.ucar.edu
+}
+
+function khaba-y {
+    ssh -Y khaba.rap.ucar.edu
+}
+
+function huni-y {
+    ssh -Y huni.rap.ucar.edu
+}
+
+function ahmose-y {
+    ssh -Y ahmose.rap.ucar.edu
+}
+
+function ncardas1-y {
+    ssh -Y ncardas1.rap.ucar.edu
+}
+
+function ncardas2-y {
+    ssh -Y ncardas2.rap.ucar.edu
+}
+
+function smac-c4 {
+    ssh smac-c4-int1.rap.ucar.edu
+}
+
+function smac-c4-y {
+    ssh -Y smac-c4-int1.rap.ucar.edu
+}
+
+function data-access {
+    ssh data-access.ucar.edu
+}
+
+
