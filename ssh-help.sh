13195b6b1830b94b21bde19585dbbb0570eed25c
Paul Prestopnik <prestop@ucar.edu>
2020-06-17 15:49:43 +0000

    list of ssh helper functions

---
+++ ssh-help.sh
@@ -0,0 +1,57 @@
+
+function kinit_atec() {
+  kinit prestop@HPCMP.HPC.MIL
+}
+
+function gordon() {
+  ssh prestop@gordon13.navydsrc.hpc.mil
+}
+
+function gaffney() {
+  ssh prestop@gaffney.navydsrc.hpc.mil
+}
+
+function excalibur() {
+  ssh atec4dwx@excalibur-4dwx.arl.hpc.mil
+}
+
+function excalibur-test() {
+  ssh atectest@excalibur-4dwx.arl.hpc.mil
+}
+
+function centennial-prestop() {
+  ssh prestop@centennial.arl.hpc.mil
+}
+
+function centennial-test() {
+  ssh atectest@centennial11.arl.hpc.mil
+}
+
+function centennial() {
+  ssh atec4dwx@centennial11.arl.hpc.mil
+}
+
+
+function atec-exposed() {
+  ssh atec-exposed.rap.ucar.edu
+}
+
+function bmb-data() {
+  ssh bigmacb-data1.rap.ucar.edu
+}
+
+function datapath1() {
+  ssh datapath1.rap.ucar.edu
+}
+
+function datapath2() {
+  ssh datapath2.rap.ucar.edu
+}
+
+function data-access() {
+  ssh data-access.ucar.edu
+}
+
+function atec-dataops4() {
+  ssh atec-dataops4.rap.ucar.edu
+}
