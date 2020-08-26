2f9d968a947360d910ca909343c1e39143f0e916
Paul Prestopnik <prestop@ucar.ucar.edu>
2020-08-26 11:14:26 -0600

    added explicit paths for ssh to differentiate between DoD and other ssh

--- ssh-help.sh
+++ ssh-help.sh
@@ -1,206 +1,212 @@
 
+# default ssh
+/usr/bin/ssh
+
+# ssh for DOD HPC 
+/usr/local/ossh/bin/ssh
+
 function kinit_atec() {
   kinit prestop@HPCMP.HPC.MIL
 }
 
 function gordon() {
-  ssh prestop@gordon13.navydsrc.hpc.mil
+  /usr/local/ossh/bin/ssh prestop@gordon13.navydsrc.hpc.mil
 }
 
 function gaffney() {
-  ssh prestop@gaffney.navydsrc.hpc.mil
+  /usr/local/ossh/bin/ssh prestop@gaffney.navydsrc.hpc.mil
 }
 
 function excalibur() {
-  ssh atec4dwx@excalibur-4dwx.arl.hpc.mil
+  /usr/local/ossh/bin/ssh atec4dwx@excalibur-4dwx.arl.hpc.mil
 }
 
 function excalibur-test() {
-  ssh atectest@excalibur-4dwx.arl.hpc.mil
+  /usr/local/ossh/bin/ssh atectest@excalibur-4dwx.arl.hpc.mil
 }
 
 function centennial-prestop() {
-  ssh prestop@centennial.arl.hpc.mil
+  /usr/local/ossh/bin/ssh prestop@centennial.arl.hpc.mil
 }
 
 function centennial-test() {
-  ssh atectest@centennial11.arl.hpc.mil
+  /usr/local/ossh/bin/ssh atectest@centennial11.arl.hpc.mil
 }
 
 function centennial() {
-  ssh atec4dwx@centennial11.arl.hpc.mil
+  /usr/local/ossh/bin/ssh atec4dwx@centennial11.arl.hpc.mil
 }
 
 function atec-exposed() {
-  ssh atec-exposed.rap.ucar.edu
+  /usr/bin/ssh atec-exposed.rap.ucar.edu
 }
 
 function bmb-data() {
-  ssh bigmacb-data1.rap.ucar.edu
+  /usr/bin/ssh bigmacb-data1.rap.ucar.edu
 }
 
 function datapath1() {
-  ssh datapath1.rap.ucar.edu
+  /usr/bin/ssh datapath1.rap.ucar.edu
 }
 
 function datapath2() {
-  ssh datapath2.rap.ucar.edu
+  /usr/bin/ssh datapath2.rap.ucar.edu
 }
 
 function data-access() {
-  ssh data-access.ucar.edu
+  /usr/bin/ssh data-access.ucar.edu
 }
 
 function atec-dataops4() {
-  ssh atec-dataops4.rap.ucar.edu
+  /usr/bin/ssh atec-dataops4.rap.ucar.edu
 }
 
 function atec-dataops3() {
-  ssh atec-dataops3.rap.ucar.edu
+  /usr/bin/ssh atec-dataops3.rap.ucar.edu
 }
 
 function deere {
-    ssh  deere.rap.ucar.edu
+    /usr/bin/ssh  deere.rap.ucar.edu
 }
 
 function thyme {
-    ssh  thyme.rap.ucar.edu
+    /usr/bin/ssh  thyme.rap.ucar.edu
 }
 
 function fip-faa {
-    ssh  fip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh  fip-faa@sonbef.rap.ucar.edu
 }
 
 function cip-faa {
-    ssh  cip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh  cip-faa@sonbef.rap.ucar.edu
 }
 
 function fip-dev {
-    ssh  fip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh  fip-dev@sonbef.rap.ucar.edu
 }
 
 function cip-dev {
-    ssh  cip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh  cip-dev@sonbef.rap.ucar.edu
 }
 
 function curry {
-    ssh  curry.rap.ucar.edu
+    /usr/bin/ssh  curry.rap.ucar.edu
 }
 
 function oregano {
-    ssh  oregano.rap.ucar.edu
+    /usr/bin/ssh  oregano.rap.ucar.edu
 }
 
 function parsley {
-    ssh  parsley.rap.ucar.edu
+    /usr/bin/ssh  parsley.rap.ucar.edu
 }
 
 function sonbef {
-    ssh  sonbef.rap.ucar.edu
+    /usr/bin/ssh  sonbef.rap.ucar.edu
 }
 
 function khaba {
-    ssh  khaba.rap.ucar.edu
+    /usr/bin/ssh  khaba.rap.ucar.edu
 }
 
 function raneb {
-    ssh  raneb.rap.ucar.edu
+    /usr/bin/ssh  raneb.rap.ucar.edu
 }
 
 function huni {
-    ssh  huni.rap.ucar.edu
+    /usr/bin/ssh  huni.rap.ucar.edu
 }
 
 function floyd {
-    ssh  floyd.rap.ucar.edu
+    /usr/bin/ssh  floyd.rap.ucar.edu
 }
 
 function ahmose {
-    ssh  ahmose.rap.ucar.edu
+    /usr/bin/ssh  ahmose.rap.ucar.edu
 }
 
 function ncardas1 {
-    ssh  ncardas1.rap.ucar.edu
+    /usr/bin/ssh  ncardas1.rap.ucar.edu
 }
 
 function ncardas2 {
-    ssh  ncardas2.rap.ucar.edu
+    /usr/bin/ssh  ncardas2.rap.ucar.edu
 }
 
 function ncardas3 {
-    ssh  ncardas3.rap.ucar.edu
+    /usr/bin/ssh  ncardas3.rap.ucar.edu
 }
 
 function deere-y {
-    ssh -Y deere.rap.ucar.edu
+    /usr/bin/ssh -Y deere.rap.ucar.edu
 }
 
 function thyme-y {
-    ssh -Y thyme.rap.ucar.edu
+    /usr/bin/ssh -Y thyme.rap.ucar.edu
 }
 
 function fip-faa-y {
-    ssh -Y fip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y fip-faa@sonbef.rap.ucar.edu
 }
 
 function cip-faa-y {
-    ssh -Y cip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y cip-faa@sonbef.rap.ucar.edu
 }
 
 function fip-dev-y {
-    ssh -Y fip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y fip-dev@sonbef.rap.ucar.edu
 }
 
 function cip-dev-y {
-    ssh -Y cip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y cip-dev@sonbef.rap.ucar.edu
 }
 
 function curry-y {
-    ssh -Y curry.rap.ucar.edu
+    /usr/bin/ssh -Y curry.rap.ucar.edu
 }
 
 function oregano-y {
-    ssh -Y oregano.rap.ucar.edu
+    /usr/bin/ssh -Y oregano.rap.ucar.edu
 }
 
 function parsley-y {
-    ssh -Y parsley.rap.ucar.edu
+    /usr/bin/ssh -Y parsley.rap.ucar.edu
 }
 
 function sonbef-y {
-    ssh -Y sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y sonbef.rap.ucar.edu
 }
 
 function khaba-y {
-    ssh -Y khaba.rap.ucar.edu
+    /usr/bin/ssh -Y khaba.rap.ucar.edu
 }
 
 function huni-y {
-    ssh -Y huni.rap.ucar.edu
+    /usr/bin/ssh -Y huni.rap.ucar.edu
 }
 
 function ahmose-y {
-    ssh -Y ahmose.rap.ucar.edu
+    /usr/bin/ssh -Y ahmose.rap.ucar.edu
 }
 
 function ncardas1-y {
-    ssh -Y ncardas1.rap.ucar.edu
+    /usr/bin/ssh -Y ncardas1.rap.ucar.edu
 }
 
 function ncardas2-y {
-    ssh -Y ncardas2.rap.ucar.edu
+    /usr/bin/ssh -Y ncardas2.rap.ucar.edu
 }
 
 function smac-c4 {
-    ssh smac-c4-int1.rap.ucar.edu
+    /usr/bin/ssh smac-c4-int1.rap.ucar.edu
 }
 
 function smac-c4-y {
-    ssh -Y smac-c4-int1.rap.ucar.edu
+    /usr/bin/ssh -Y smac-c4-int1.rap.ucar.edu
 }
 
 function data-access {
-    ssh data-access.ucar.edu
+    /usr/bin/ssh data-access.ucar.edu
 }
 
 
