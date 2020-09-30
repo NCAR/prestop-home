bde7788adf77dd54b4b4f4d49bb7bd89b9eead82
Paul Prestopnik <paul@prestopnik.com>
2020-09-30 13:29:50 -0600

    added explicit user names

--- ssh-help.sh
+++ ssh-help.sh
@@ -72,179 +72,179 @@ fi
 }
 
 atec-exposed() {
-  /usr/bin/ssh atec-exposed.rap.ucar.edu
+  /usr/bin/ssh prestop@atec-exposed.rap.ucar.edu
 }
 
 bmb-data() {
-  /usr/bin/ssh bigmacb-data1.rap.ucar.edu
+  /usr/bin/ssh prestop@bigmacb-data1.rap.ucar.edu
 }
 
 datapath1() {
-  /usr/bin/ssh datapath1.rap.ucar.edu
+  /usr/bin/ssh prestop@datapath1.rap.ucar.edu
 }
 
 datapath2() {
-  /usr/bin/ssh datapath2.rap.ucar.edu
+  /usr/bin/ssh prestop@datapath2.rap.ucar.edu
 }
 
 datapath-dev() {
-  /usr/bin/ssh datapath-dev.rap.ucar.edu
+  /usr/bin/ssh prestop@datapath-dev.rap.ucar.edu
 }
 
 data-access() {
-  /usr/bin/ssh data-access.ucar.edu
+  /usr/bin/ssh prestop@data-access.ucar.edu
 }
 
 atec-dataops4() {
-  /usr/bin/ssh atec-dataops4.rap.ucar.edu
+  /usr/bin/ssh prestop@atec-dataops4.rap.ucar.edu
 }
 
 atec-dataops3() {
-  /usr/bin/ssh atec-dataops3.rap.ucar.edu
+  /usr/bin/ssh prestop@atec-dataops3.rap.ucar.edu
 }
 
 deere() {
-    /usr/bin/ssh  deere.rap.ucar.edu
+    /usr/bin/ssh prestop@deere.rap.ucar.edu
 }
 
 thyme() {
-    /usr/bin/ssh  thyme.rap.ucar.edu
+    /usr/bin/ssh prestop@thyme.rap.ucar.edu
 }
 
 fip-faa() {
-    /usr/bin/ssh  fip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh fip-faa@sonbef.rap.ucar.edu
 }
 
 cip-faa() {
-    /usr/bin/ssh  cip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh cip-faa@sonbef.rap.ucar.edu
 }
 
 fip-dev() {
-    /usr/bin/ssh  fip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh fip-dev@sonbef.rap.ucar.edu
 }
 
 cip-dev() {
-    /usr/bin/ssh  cip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh cip-dev@sonbef.rap.ucar.edu
 }
 
 curry() {
-    /usr/bin/ssh  curry.rap.ucar.edu
+    /usr/bin/ssh prestop@curry.rap.ucar.edu
 }
 
 oregano() {
-    /usr/bin/ssh  oregano.rap.ucar.edu
+    /usr/bin/ssh prestop@oregano.rap.ucar.edu
 }
 
 parsley() {
-    /usr/bin/ssh  parsley.rap.ucar.edu
+    /usr/bin/ssh prestop@parsley.rap.ucar.edu
 }
 
 sonbef() {
-    /usr/bin/ssh  sonbef.rap.ucar.edu
+    /usr/bin/ssh prestop@sonbef.rap.ucar.edu
 }
 
 khaba() {
-    /usr/bin/ssh  khaba.rap.ucar.edu
+    /usr/bin/ssh prestop@khaba.rap.ucar.edu
 }
 
 raneb() {
-    /usr/bin/ssh  raneb.rap.ucar.edu
+    /usr/bin/ssh prestop@raneb.rap.ucar.edu
 }
 
 huni() {
-    /usr/bin/ssh  huni.rap.ucar.edu
+    /usr/bin/ssh prestop@huni.rap.ucar.edu
 }
 
 floyd() {
-    /usr/bin/ssh  floyd.rap.ucar.edu
+    /usr/bin/ssh prestop@floyd.rap.ucar.edu
 }
 
 ahmose() {
-    /usr/bin/ssh  ahmose.rap.ucar.edu
+    /usr/bin/ssh prestop@ahmose.rap.ucar.edu
 }
 
 ncardas1() {
-    /usr/bin/ssh  ncardas1.rap.ucar.edu
+    /usr/bin/ssh prestop@ncardas1.rap.ucar.edu
 }
 
 ncardas2() {
-    /usr/bin/ssh  ncardas2.rap.ucar.edu
+    /usr/bin/ssh prestop@ncardas2.rap.ucar.edu
 }
 
 ncardas3() {
-    /usr/bin/ssh  ncardas3.rap.ucar.edu
+    /usr/bin/ssh prestop@ncardas3.rap.ucar.edu
 }
-
+ 
 deere-y() {
-    /usr/bin/ssh -Y deere.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@deere.rap.ucar.edu
 }
 
 thyme-y() {
-    /usr/bin/ssh -Y thyme.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@thyme.rap.ucar.edu
 }
 
 fip-faa-y() {
-    /usr/bin/ssh -Y fip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@fip-faa@sonbef.rap.ucar.edu
 }
 
 cip-faa-y() {
-    /usr/bin/ssh -Y cip-faa@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@cip-faa@sonbef.rap.ucar.edu
 }
 
 fip-dev-y() {
-    /usr/bin/ssh -Y fip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@fip-dev@sonbef.rap.ucar.edu
 }
 
 cip-dev-y() {
-    /usr/bin/ssh -Y cip-dev@sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@cip-dev@sonbef.rap.ucar.edu
 }
 
 curry-y() {
-    /usr/bin/ssh -Y curry.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@curry.rap.ucar.edu
 }
 
 oregano-y() {
-    /usr/bin/ssh -Y oregano.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@oregano.rap.ucar.edu
 }
 
 parsley-y() {
-    /usr/bin/ssh -Y parsley.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@parsley.rap.ucar.edu
 }
 
 sonbef-y() {
-    /usr/bin/ssh -Y sonbef.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@sonbef.rap.ucar.edu
 }
 
 khaba-y() {
-    /usr/bin/ssh -Y khaba.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@khaba.rap.ucar.edu
 }
 
 huni-y() {
-    /usr/bin/ssh -Y huni.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@huni.rap.ucar.edu
 }
 
 ahmose-y() {
-    /usr/bin/ssh -Y ahmose.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@ahmose.rap.ucar.edu
 }
 
 ncardas1-y() {
-    /usr/bin/ssh -Y ncardas1.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@ncardas1.rap.ucar.edu
 }
 
 ncardas2-y() {
-    /usr/bin/ssh -Y ncardas2.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@ncardas2.rap.ucar.edu
 }
 
 smac-c4() {
-    /usr/bin/ssh smac-c4-int1.rap.ucar.edu
+    /usr/bin/ssh prestop@smac-c4-int1.rap.ucar.edu
 }
 
 smac-c4-y() {
-    /usr/bin/ssh -Y smac-c4-int1.rap.ucar.edu
+    /usr/bin/ssh -Y prestop@smac-c4-int1.rap.ucar.edu
 }
 
 data-access() {
-    /usr/bin/ssh data-access.ucar.edu
+    /usr/bin/ssh prestop@data-access.ucar.edu
 }
 
 
