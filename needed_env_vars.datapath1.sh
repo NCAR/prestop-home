4892b671fed62291061f59420c1bf9d525671184
Paul Prestopnik <prestop@ucar.edu>
2020-10-22 22:12:43 +0000

    env vars specifically for datapath1

---
+++ needed_env_vars.datapath1.sh
@@ -0,0 +1,13 @@
+
+# add location of gcp & python3 and globus cli
+
+PATH=$(path_prepend $PATH /opt/globusconnectpersonal )
+
+PATH=$(path_prepend $PATH /opt/python3/bin )
+
+PATH=$(path_prepend $PATH $HOME/archive/bin )
+
+
+
+
+
